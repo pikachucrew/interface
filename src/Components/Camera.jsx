290c4bc335b19b02627385a319c21d42a6b5840d
Rose Mullan <rosemullan@Roses-MacBook-Air.local>
2020-01-27 15:49:45 +0000

    initiated webcam app

--- src/Components/Camera.jsx
+++ src/Components/Camera.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Camera() {
   return (
     <div>
+      This is where the camera will  go
       <video id="camera" autoPlay>
         {" "}
       </video>
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Camera from "./Camera";
-import Webcamera from "./Webcamera"
+import PictureBooth from "./Picturebooth"
 
 function Dashboard() {
   return (
     <div id="Dashboard">
       <h1>Today you are: happy</h1>
       {/* <Camera /> */}
-      <Webcamera />
+      <WebCamera />
     </div>
   );
 }
