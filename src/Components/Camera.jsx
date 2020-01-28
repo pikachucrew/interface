4a8ca33bec1d382192fe13d2c576be8f3a681c84
Rose Mullan <rosemullan@Roses-MacBook-Air.local>
2020-01-28 09:42:23 +0000

    add camera button

--- src/Components/Camera.jsx
+++ src/Components/Camera.jsx
@@ -3,10 +3,11 @@ import React from "react";
 function Camera() {
   return (
     <div>
-      This is where the camera will  go
+      This is where the camera will go
       <video id="camera" autoPlay>
         {" "}
       </video>
+      <button>Start Camera</button>
     </div>
   );
 }
